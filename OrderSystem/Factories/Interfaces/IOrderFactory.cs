cc06ddf3009622707c0aa78fbd7924bcdb40c962
ArtemkaGoldMan <artemka.omelchenko@gmail.com>
2025-03-16 16:25:29 +0100

    OrderFactory added

---
+++ OrderSystem/Factories/Interfaces/IOrderFactory.cs
@@ -0,0 +1,14 @@
+﻿using OrderSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderSystem.Factories.Interfaces
+{
+    public interface IOrderFactory
+    {
+        Order CreateOrder(string productName, decimal amount, CustomerType customer, string deliveryAddress, PaymentMethod payment);
+    }
+}
